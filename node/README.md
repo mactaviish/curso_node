530615907251b44290f6ec98bd870268dd7cd0e4
Leonardo Dalla Corte <37817455+mactaviish@users.noreply.github.com>
2022-09-01 16:53:57 -0300

    add

---
+++ node/README.md
@@ -0,0 +1,1 @@
+Curso (Node.js: API Rest com Express e MongoDB)[https://cursos.alura.com.br/course/nodejs-api-rest-express-mongodb]
---
+++ node/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const livros = [
+    {id: 1, "titulo": "Harry Potter"},
+    {id: 2, "titulo": "Game of Thrones"}
+];
+
+app.get('/', (req, res) => {
+    res.status(200).send('Curso de Node com Express e MongoDB');
+});
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro cadastrado com sucesso!');
+});
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+});
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id == id);
+}
+
+export default app;
